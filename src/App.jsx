9f480521ef305baa3e2c75a861d5f364882052de
Tiago Santana Palmeira <125421932+Tiagopalmeira@users.noreply.github.com>
2024-08-28 11:25:58 -0300

    V1 do projeto

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import Header from './components/header'
+import Home from './components/home'
+import Footer from './components/footer';
+
+
+function App() {
+  return (
+
+    <div>
+
+      <Header />
+      <Home />
+      <Footer />
+      <div className='certificado'>
+        <img src="https://acdn.mitiendanube.com/stores/001/686/454/themes/amazonas/1-img-1638445021-1632833679-5e978ed505aa23e646d67624ee06a0f31632833679-480-0.webp?46755272" alt="" />
+
+      </div>
+    </div>
+
+  );
+}
+
+export default App
